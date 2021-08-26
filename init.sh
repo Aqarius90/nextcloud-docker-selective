8c23ef1b0c6dcae07a51dbd2bb93bfc8e6f43835
Aqarius90 <aqarius90@gmail.com>
2021-08-26 14:45:32 +0200

    add docker-compose, test bash syntax

--- init.sh
+++ init.sh
@@ -1,16 +1,25 @@
-if [ ${#IN[@]} -ne ${#OUT[@]}] ; then
-  echo "[ERR] IN/OUT mismatch" | ts "${LOG_DATE_FORMAT}"
+#!/bin/bash
+IN=("ss1w", "2", "3", "4");
+OUT=("2", "3", "4", "0");
+echo ${#IN[@]}
+echo ${#OUT[@]}
+if [ ${#IN[@]} -ne ${#OUT[@]} ];
+then
+  echo "[ERR] IN/OUT mismatch"
   exit 1
 fi
 
-while true
-do
-	for ((i = 0 ; i <= ${#IN[@]} ; i++)); do
-  		echo "$IN[$i]"
-		echo "$OUT[$i]"
+#while true
+#do
+	for ((i = 0 ; i <= ${#IN[@]}-1 ; i++)); do
+		echo "Sync ${IN[$i]} to ${OUT[$i]}"
+		nextcloudcmd $( [ "$MUTE" ] && echo "--silent" ) \
+			-u ${USER} -p ${PASS} \
+			/media/nextcloud/${IN[$i]} \
+			 ${URL}/nextcloud/remote.php/webdav/${OUT[$i]}
 	done;
 	#nextcloudcmd $( [ "$MUTE" == true ] && echo "--silent" )\
 	#	-u $USER -p $PASS /media/nextcloud/ $URL\
-	echo "Next sync in ${SYNC}"
+	echo "Next sync in ${SLEEP}"
 	sleep $SLEEP
-done
+#done
